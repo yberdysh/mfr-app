575dc2e83664791dce4590e62bc41f4146d62955
yberdysh <yberdysh@gmail.com>
2025-03-07 22:46:37 -0500

    background-image, navbar, colors

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,13 +5,13 @@ function NavBar() {
   return (
     <div className="navbar-container">
       <nav className="navbar">
-        <div className="navbar-brand">MyApp</div>
+        <div className="navbar-brand">Myofascial Awakening</div>
         <div className="navbar-links">
           <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#services">Services</a>
           <a href="#contact">Contact</a>
-          <button className="action-button">Sign Up</button>
+          <button className="action-button">Book Now</button>
         </div>
       </nav>
     </div>
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,15 @@
-import "./index.scss";
+import './index.scss';
+import treatment from '../../assets/treatment-room.jpeg';
 
 function HomePage() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
+  return (
+    <div className="home-page">
+      {/* <h1>Home Page</h1> */}
+      <div>
+        <img className="banner-image" src={treatment} alt="treatment room" />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
