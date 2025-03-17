c24dff25b15c0366050e52e2857bde3121e5704d
Joe Costa <joecosta@Joes-MacBook-Air.local>
2025-03-17 13:35:14 -0400

    updating the nav on desktop/laptop, mobile next

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,24 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import Navbar from './components/NavBar';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import Navbar from "./components/NavBar";
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <ScrollToTop>
+        <div id="page-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </ScrollToTop>
     </Router>
-    // <>
-    //   <div>
-    //     <a href="https://vite.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
   );
 }
 
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './index.scss';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./index.scss";
 
 function NavBar() {
+  // Adjust content height depending on the nav bar height
+  const adjustContentMarginTop = () => {
+    const navBar = document.getElementById("nav");
+    const content = document.getElementById("page-content");
+
+    console.log("nav: ", nav, "content", content);
+
+    if (navBar && content) {
+      content.style.marginTop = `${navBar.offsetHeight}px`;
+    }
+  };
+
+  // useEffect to adjust the page content spacing during resizing
+  useEffect(() => {
+    adjustContentMarginTop();
+
+    window.addEventListener("resize", adjustContentMarginTop);
+    return () => {
+      window.removeEventListener("resize", adjustContentMarginTop);
+    };
+  }, []);
+
+  function handleBooking() {
+    // booking logic goes here...
+  }
+
   return (
-    <div className="navbar-container">
+    <div className="navbar-container" id="nav">
       <nav className="navbar">
-        <div className="navbar-brand">Myofascial Awakening</div>
+        <div className="navbar-brand">
+          <Link to="/">Myofascial Awakening</Link>
+        </div>
         <div className="navbar-links">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <a href="#services">Services</a>
+          <a href="#services">What is MFR?</a>
           <a href="#contact">Contact</a>
-          <button className="action-button">Book Now</button>
+          <button className="action-button" onClick={handleBooking}>
+            Book Now
+          </button>
         </div>
       </nav>
     </div>
